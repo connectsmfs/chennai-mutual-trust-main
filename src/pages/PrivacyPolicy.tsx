fa756720441e894a58a8c004e2941e8567ee35ff
connectsmfs <connect@shreemfs.com>
2025-11-07 13:12:44 +0530

    Add PrivacyPolicy.tsx

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -1,5 +1,6 @@
-// src/pages/PrivacyPolicy.tsx
 import React from "react";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const PrivacyPolicy = () => {
   return (

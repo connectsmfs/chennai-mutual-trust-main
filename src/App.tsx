26322dd51f118eaa0fc0df61af0c1ec1c3bf8550
connectsmfs <connect@shreemfs.com>
2025-11-07 12:49:36 +0530

    Add PrivacyPolicy.tsx and update App.tsx routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PrivacyPolicy from "./pages/PrivacyPolicy"; 
+import PrivacyPolicy from "./pages/PrivacyPolicy"; // no .js extension
 
 import Index from "./pages/Index";
 import AboutPage from "./pages/AboutPage";
